e2ae9464e1459f6199026c12616b672d17b468e4
Jaimeson Waugh <jaimeson@4cesi.com>
2019-05-02 16:44:14 -0700

    Start working on basic image recognition test

--- src/TestImageAltTags.php
+++ src/TestImageAltTags.php
@@ -5,8 +5,13 @@ namespace WaughJ\TestImageAltTags
 {
 	class TestImageAltTags
 	{
-		public function __construct()
+		public function __construct( string $html )
 		{
+		}
+
+		public function hasImages() : bool
+		{
+			return false;
 		}
 	}
 }
--- tests/TestImageAltTagsTest.php
+++ tests/TestImageAltTagsTest.php
@@ -5,9 +5,11 @@ use WaughJ\TestImageAltTags\TestImageAltTags;
 
 class TestImageAltTagsTest extends TestCase
 {
-	public function testObjectWorks()
+	public function testImageRecognition()
 	{
-		$object = new TestImageAltTags();
-		$this->assertTrue( is_object( $object ) );
+		$test = new TestImageAltTags( 'ajhsfhsdkfhsadkfh' );
+		$this->assertFalse( $test->hasImages() );
+		$test = new TestImageAltTags( '<img src="something.png">' );
+		$this->assertTrue( $test->hasImages() );
 	}
 }
